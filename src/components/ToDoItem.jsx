2e00fb261c221e477c92d56b69ebe851a4a86b8b
DustinMcMullen <51675499+DustinMcMullen@users.noreply.github.com>
2020-09-05 16:30:25 -0500

    added useState within components

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,7 +1,21 @@
 import React, { useState } from "react";
 
 function ToDoItem(props) {
-  return <li>{props.text}</li>;
+  const [completed, setCompleted] = useState(null);
+
+  function handleTask() {
+    if (completed === null) {
+      setCompleted("line-through");
+    } else if (completed === "line-through") {
+      setCompleted(null);
+    }
+  }
+
+  return (
+    <li style={{ textDecoration: completed }} onMouseDown={handleTask}>
+      {props.text}
+    </li>
+  );
 }
 
 export default ToDoItem;
