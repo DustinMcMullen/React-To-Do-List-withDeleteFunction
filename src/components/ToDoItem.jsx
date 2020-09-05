bcb138158133f8cab40bf967d853be97e282e190
DustinMcMullen <51675499+DustinMcMullen@users.noreply.github.com>
2020-09-05 18:27:57 -0500

    cleaned up & refactored code

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,20 +1,9 @@
 import React, { useState } from "react";
 
 function ToDoItem(props) {
-  const [completed, setCompleted] = useState(null);
-
-  function handleTask() {
-    if (completed === null) {
-      setCompleted("line-through");
-    } else if (completed === "line-through") {
-      setCompleted(null);
-    }
-  }
-
   return (
     <div>
       <li
-        style={{ textDecoration: completed }}
         onMouseDown={() => {
           props.delete(props.id);
         }}
